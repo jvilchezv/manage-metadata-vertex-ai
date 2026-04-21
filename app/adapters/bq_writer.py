fbc8f3a1b012835ea287d1585fa3835ec2443182
jvilchezv <joel.vilchez@rimac.com.pe>
2026-04-21 11:46:14 -0500

    fix: update model

--- app/adapters/bq_writer.py
+++ app/adapters/bq_writer.py
@@ -51,10 +51,8 @@ def update_table_schema(metadata: Dict[str, Any], client: bigquery.Client) -> No
         (metadata.get("table_description") or {}).get("description") or ""
     ).strip()
     current_table_description = (table.description or "").strip()
-    print(current_table_description)
     table_desc_changed = current_table_description != new_table_description
     print(table_desc_changed)
-    print(new_table_description)
 
     if table_desc_changed:
         logger.info(
